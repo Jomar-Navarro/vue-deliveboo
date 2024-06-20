578cbde7ab767950ff83afb1387996893f47c180
simoneaucello <simoneaucello@gmail.com>
2024-06-20 17:53:09 +0200

    [add] jumbotron

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/scss/main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import '@fontsource/bungee-shade';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import '@fontsource/luckiest-guy';
 import { router } from "./router";
 
 createApp(App).use(router).mount("#app");
