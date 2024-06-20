cac24553a75924910fe38273e7833254ccb99138
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-20 11:17:43 +0200

    [add] router view

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import App from "./App.vue";
 import "./assets/scss/main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { router } from "./router";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+	name: "Home",
+};
+</script>
+
+<template>
+	<div><h1>Home</h1></div>
+</template>
+
+<style lang="scss" scoped></style>
