f26462f6dfe325fafd3b1a9d671d8470219d1555
simoneaucello <simoneaucello@gmail.com>
2024-06-21 09:57:54 +0200

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -62,9 +62,13 @@ header {
 		color: #ff9f22;
 	}
 	.link-title {
-		color: #ff9f22;
+		color: #ffc107;
 		font-size: 1.5rem;
 		letter-spacing: 1px;
+		transition: all .3s ease-out;
+		&:hover{
+			color: rgb(251, 198, 145);
+		}
 	}
 
 	.logo {
