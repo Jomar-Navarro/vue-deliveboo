aa506ca54f57146b870323c39e4f798129de2e28
JacopoLombardi <lombajacopo@gmail.com>
2024-06-24 17:27:14 +0200

    [fix] Header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,101 +1,103 @@
 <script>
 import Cart from "./partials/Cart.vue";
+
 export default {
 	components: {
 		Cart,
+	},
+	data() {
+		return {
+			isMenuOpen: false,
+		};
+	},
+	methods: {
+		toggleMenu() {
+			this.isMenuOpen = !this.isMenuOpen;
+		},
 	},
 };
 </script>
 
 <template>
-	<header class="px-3 position-sticky w-100 top-0 z-1">
-		<!-- <nav class="navbar navbar-expand-lg mx-3"> -->
-		<div
-			class="d-flex align-items-center justify-content-between justify-content-center h-100"
-		>
-			<div class="">
-				<img src="/img/logo-final.png" alt="" class="logo" />
-				<!-- <h1>Logo</h1> -->
+	<header class="header bg-dark sticky-top py-2">
+		<div class="container-fluid d-flex align-items-center justify-content-between">
+			<div class="logo-container">
+				<img src="/img/logo-final.png" alt="Logo" class="logo img-fluid" />
 			</div>
 
-			<div class="d-flex align-items-center h-100">
-				<ul class="d-flex justify-content-between align-items-center h-100 m-0">
+			<nav class="font_ nav d-none d-md-flex flex-grow-1 justify-content-center">
+				<ul class="nav-list d-flex list-unstyled mb-0">
 					<li class="nav-item">
-						<router-link class="nav-link link-title p-3" :to="{ name: 'home' }"
-							>Home</router-link
-						>
+						<router-link class="nav-link text-warning" :to="{ name: 'home' }">Home</router-link>
 					</li>
-
 					<li class="nav-item">
-						<router-link
-							class="nav-link link-title p-3"
-							:to="{ name: 'contacts' }"
-							>Contatti</router-link
-						>
+						<router-link class="nav-link text-warning" :to="{ name: 'contacts' }">Contatti</router-link>
 					</li>
-
 					<li class="nav-item">
-						<router-link class="nav-link link-title p-3" :to="{ name: 'about' }"
-							>Chi Siamo</router-link
-						>
+						<router-link class="nav-link text-warning" :to="{ name: 'about' }">Chi Siamo</router-link>
 					</li>
 				</ul>
-			</div>
+			</nav>
 
-			<div class="ms-5">
-				<div class="text-white d-flex align-items-center">
-					<div class="bottone btn  me-3">
-						<button
-							class="btn btn-outline-warning"
-							type="button"
-							data-bs-toggle="offcanvas"
-							data-bs-target="#offcanvasRight"
-							aria-controls="offcanvasRight"
-						>
-							Vai al carrello
-						</button>
-					</div>
-					<div class="text-warning fs-4">
-						<i class="fa-solid fa-cart-shopping"></i>
-					</div>
+			<div class="cart-container d-flex align-items-center me-md-4">
+				<button class="font_ btn btn-outline-warning d-none d-md-flex me-2" type="button" data-bs-toggle="offcanvas"
+					data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+					Vai al carrello
+				</button>
+				<div class="cart-icon">
+					<a class="text-warning" href="" data-bs-toggle="offcanvas"
+					data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="fa-solid fa-cart-shopping"></i></a>
 				</div>
 			</div>
 
-			<Cart />
+			<div class="hamburger-icon text-white d-md-none">
+				<i class="fa-solid fa-bars me-4" @click="toggleMenu"></i>
+			</div>
 		</div>
-		<!-- </nav> -->
+
+		<nav class="font_ nav d-md-none flex-column" :class="{ 'd-flex': isMenuOpen, 'd-none': !isMenuOpen }">
+			<ul class="nav-list list-unstyled text-center m-0">
+				<li class="nav-item">
+					<router-link class="nav-link text-warning" :to="{ name: 'home' }">Home</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="nav-link text-warning" :to="{ name: 'contacts' }">Contatti</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="nav-link text-warning" :to="{ name: 'about' }">Chi Siamo</router-link>
+				</li>
+			</ul>
+		</nav>
+
+		<Cart />
 	</header>
 </template>
 
+
 <style lang="scss" scoped>
-header {
-	background-color: #292626;
-	height: 90px;
 
-	li {
-		list-style: none;
-		font-family: "Luckiest Guy", system-ui;
-		color: #ff9f22;
+.header {
+	.logo {
+		width: 120px;
 	}
-
-	.link-title {
-		color: #ffc107;
+	.nav-link {
 		font-size: 1.5rem;
 		letter-spacing: 1px;
 		transition: all 0.3s ease-out;
-
 		&:hover {
 			color: rgb(251, 198, 145);
 		}
 	}
-
-	.logo {
-		width: 150px;
-		object-fit: contain;
-	}
-
-	.bottone {
+	.font_ {
 		font-family: "Luckiest Guy", system-ui;
 	}
+	.fa-solid {
+		font-size: 2rem;
+	}
+	.hamburger-icon {
+		cursor: pointer;
+		font-size: 2rem;
+	}
 }
+
 </style>
