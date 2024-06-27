5e08bd56005c42cf9b1c27294394f5666aea6aa8
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-27 17:35:34 +0200

    [add] fix links

--- src/pages/Contatti.vue
+++ src/pages/Contatti.vue
@@ -1,74 +1,69 @@
 <script>
 export default {
-  name: "contacts",
+	name: "contacts",
 };
 </script>
 
 <template>
-  <div class="contacts">
-    <h1>Contatti</h1>
-    <section class="contact-info">
-      <h2>Informazioni di Contatto</h2>
-      <p>
-        Per qualsiasi domanda o informazione, puoi contattarci tramite uno dei seguenti metodi. Siamo qui per aiutarti!
-      </p>
-      <ul>
-        <li>
-          <strong>Indirizzo:</strong> Via Monte Napoleone, 12, 20121 Milano MI, Italia
-        </li>
-        <li>
-          <strong>Telefono:</strong> +39 02 1234 5678
-        </li>
-        <li>
-          <strong>Email:</strong> supporto@deliveboo.com
-        </li>
-      </ul>
-    </section>
-    
-  </div>
+	<div class="container pt-5">
+		<div class="contacts mt-5 pt-5">
+			<div class="my-5">
+				<h1>Contatti</h1>
+				<section class="contact-info">
+					<h2>Informazioni di Contatto</h2>
+					<p>
+						Per qualsiasi domanda o informazione, puoi contattarci tramite uno
+						dei seguenti metodi. Siamo qui per aiutarti!
+					</p>
+					<ul>
+						<li>
+							<strong>Indirizzo:</strong> Via Monte Napoleone, 12, 20121 Milano
+							MI, Italia
+						</li>
+						<li><strong>Telefono:</strong> +39 02 1234 5678</li>
+						<li><strong>Email:</strong> supporto@deliveboo.com</li>
+					</ul>
+				</section>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-div{
-	margin: 40px;
-	padding-top: 40px;
-}
 .contacts {
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #fff;
+	margin: 0 auto;
+	padding: 20px;
+	background-color: #fff;
 	border: none;
 
-  h1, h2 {
-    color: #333;
-  }
+	h1,
+	h2 {
+		color: #333;
+	}
 
-  h1 {
-		color: #FFC108;
-    font-size: 2.5rem;
-    margin-bottom: 1.5rem;
-  }
+	h1 {
+		color: #ffc108;
+		font-size: 2.5rem;
+		margin-bottom: 1.5rem;
+	}
 
-  h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
+	h2 {
+		font-size: 2rem;
+		margin-bottom: 1rem;
+	}
 
+	ul {
+		list-style: none;
+		padding: 0;
 
+		li {
+			font-size: 1.1rem;
+			margin-bottom: 0.5rem;
 
-  ul {
-    list-style: none;
-    padding: 0;
-    
-    li {
-      font-size: 1.1rem;
-      margin-bottom: 0.5rem;
-      
-      strong {
-        color: #333;
-      }
-    }
-  }
-
-  }
+			strong {
+				color: #333;
+			}
+		}
+	}
+}
 </style>
