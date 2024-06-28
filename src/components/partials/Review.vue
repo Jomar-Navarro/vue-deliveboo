20f31da4f4adbfc6313602171605e2a0fadb3d34
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-29 00:29:55 +0200

    [add] section

--- src/components/partials/Review.vue
+++ src/components/partials/Review.vue
@@ -1,51 +1,112 @@
-<template>
-	<div class="container-fluid">
-		<h1 class="text-center mt-4 mb-4">Recensioni</h1>
+<script>
+export default {};
+</script>
 
-		<div class="row justify-content-center flex-column align-items-center">
-			<div class="col-lg-6 col-md-8">
-				<div class="card mb-3">
-					<div class="row no-gutters">
-						<div class="col-md-4">
-							<img src="" class="card-img" alt="Persona che mangia" />
-						</div>
-						<div class="col-md-8">
-							<div class="card-body">
-								<p class="card-text">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-									feugiat massa eget tellus euismod, ac bibendum ipsum
-									ultricies.
-								</p>
-								<p class="card-text">
-									<small class="text-muted">Nome della persona</small>
-								</p>
+<template>
+	<section id="reviews" class="py-5">
+		<div class="container-fluid">
+			<h1 class="text-center py-4">Recensioni</h1>
+
+			<div class="row justify-content-center flex-column align-items-center">
+				<div class="col-lg-6 col-md-8 my-5">
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4">
+								<img src="" class="card-img" alt="Persona che mangia" />
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<p class="card-text">
+										"Grazie a questa app ho scoperto ristoranti fantastici! Ho
+										ordinato il mio pasto preferito senza problemi e la consegna
+										è stata rapida. Consiglio vivamente a tutti di provare
+										questa piattaforma."
+									</p>
+									<p class="card-text">
+										<small class="text-muted">Ugo De Ughi</small>
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-6 col-md-8">
-				<div class="card mb-3">
-					<div class="row no-gutters">
-						<div class="col-md-4 order-md-2">
-							<img src="" class="card-img" alt="Persona che mangia" />
+				<div class="col-lg-6 col-md-8 my-5">
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4 order-md-2">
+								<img src="" class="card-img" alt="Persona che mangia" />
+							</div>
+							<div class="col-md-8 order-md-1">
+								<div class="card-body">
+									<p class="card-text">
+										"Ottima esperienza! Ho scoperto nuovi ristoranti e il
+										servizio è stato impeccabile. L'applicazione è facile da
+										usare e mi ha permesso di ordinare cibo delizioso con
+										facilità."
+									</p>
+									<p class="card-text">
+										<small class="text-muted">Anna Rossi</small>
+									</p>
+								</div>
+							</div>
 						</div>
-						<div class="col-md-8 order-md-1">
-							<div class="card-body">
-								<p class="card-text">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-									feugiat massa eget tellus euismod, ac bibendum ipsum
-									ultricies.
-								</p>
-								<p class="card-text">
-									<small class="text-muted">Nome della persona</small>
-								</p>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-8 my-5">
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4">
+								<img src="" class="card-img" alt="Persona che mangia" />
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<p class="card-text">
+										"Sono rimasta molto soddisfatta dell'applicazione. Mi ha
+										aiutato a trovare ristoranti che offrono piatti deliziosi e
+										la possibilità di ordinare comodamente da casa. Davvero
+										pratica e intuitiva."
+									</p>
+									<p class="card-text">
+										<small class="text-muted">Luca Verdi</small>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-8 my-5">
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4 order-md-2">
+								<img src="" class="card-img" alt="Persona che mangia" />
+							</div>
+							<div class="col-md-8 order-md-1">
+								<div class="card-body">
+									<p class="card-text">
+										"Fantastica app! Ho potuto esplorare una varietà di
+										ristoranti nella mia zona e ho sempre ricevuto il mio ordine
+										puntualmente. Perfetta per chi ama ordinare cibo di qualità
+										con semplicità."
+									</p>
+									<p class="card-text">
+										<small class="text-muted">Sofia Bianchi</small>
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 </template>
+
+<style lang="scss" scoped>
+#reviews {
+	background-color: #f5e7c1;
+	height: auto;
+}
+</style>
